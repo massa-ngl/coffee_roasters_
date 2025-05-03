ad5797a2b68fad7891a0e5caa639a09478b9c790
massa-ngl <blaise.nerd01@gmail.com>
2025-05-03 16:49:57 +0100

    modified index.js

--- starter-code/index.js
+++ starter-code/index.js
@@ -4,3 +4,18 @@ menuButton.onclick=function(){
     let navBar=document.querySelector("#navMenu");
     navBar.classList.toggle("active");
 };
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
